261a224310db23189454ea0269c148ea208d8c23
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2023-01-07 02:56:32 +0530

    [Kavyasree J ganesh] Census analyzer

--- src/test/java/com/bridgelabz/TestCSVStateCensus.java
+++ src/test/java/com/bridgelabz/TestCSVStateCensus.java
@@ -18,11 +18,24 @@ public class TestCSVStateCensus {
         }
     }
 
+    // File name different
     @Test
-    public void testReadDataFromDestinationSad(){
+    public void testReadDataFromDestinationSad1(){
         csvStateCensus =  new CSVStateCensus();
         try{
             ArrayList<String> stateList = csvStateCensus.readDataFromSource("C:\\Day_29_Census_Analyzer\\src\\main\\java\\com\\bridgelabz\\State.csv");
+            fail("Should throw exception");
+        } catch(Exception e){
+            Assertions.assertNotNull(e.getMessage());
+        }
+    }
+
+    // Incorrect file extension
+    @Test
+    public void testReadDataFromDestinationSad2(){
+        csvStateCensus =  new CSVStateCensus();
+        try{
+            ArrayList<String> stateList = csvStateCensus.readDataFromSource("C:\\Day_29_Census_Analyzer\\src\\main\\java\\com\\bridgelabz\\State.txt");
             fail("Should throw exception");
         } catch(Exception e){
             Assertions.assertNotNull(e.getMessage());
